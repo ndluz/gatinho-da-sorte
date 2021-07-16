9d3bbd5f289b85cccb1fcddb41ee5ccf0184cc53
Anderson Luz <anderson54644@gmail.com>
2021-07-15 22:15:58 -0300

    fetch

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
-const express = require('express')
-const path = require('path')
+const express = require('express');
+const path = require('path');
+
 const app = express();
   port = process.env.PORT || 3000
-const routes = ('routes');
 
 app.use(express.static(path.resolve(__dirname, 'src')));
 
 app.get('/', function(req, res) {
   res.render('./src/index')
-})
+});
 app.listen(port);
